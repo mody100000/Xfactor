19c25a8478cf2e25fda37b70b45a7fc0f5b00533
mahmoud mohamed gomaa <m.anamody10000@gmail.com>
2024-06-06 13:03:20 +0300

    adding dashboard layout & dashboard router

---
+++ src/pages/DashboardProfile/Dashboard.jsx
@@ -0,0 +1,10 @@
+import MainContent from '../../components/Dashboard/MainContent/MainContent';
+
+const Dashboard = () => {
+   
+    return (
+          <MainContent />
+    );
+  };
+  
+export default Dashboard;
