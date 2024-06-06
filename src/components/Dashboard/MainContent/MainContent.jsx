17a0f38a11cc49b4c997399f70adee2ea7c475c1
mahmoud mohamed gomaa <m.anamody10000@gmail.com>
2024-06-06 12:45:30 +0300

    fainting profile page

--- src/components/Dashboard/MainContent/MainContent.jsx
+++ src/components/Dashboard/MainContent/MainContent.jsx
@@ -3,6 +3,7 @@ import styles from './MainContent.module.css';
 import Card from './../common/Card/Card';
 import ChartComponent from '../common/Chart/ChartComponent/ChartComponent';
 import CertificateComponent from '../common/Chart/CertificateComponent/CertificateComponent';
+import Views from './../Views/Views';
 
 const MainContent = () => {
   return (
@@ -23,23 +24,23 @@ const MainContent = () => {
         </div>
       </div>
       <div className={`${styles.certificates} row`}>
-        <Card title="Graphic Design" type="specialization" className="col-md-6 col-lg-3" />
-        <Card title="User Experience Research and Design" type="specialization" className="col-md-6 col-lg-3" />
-        <Card title="Innovation through design" type="course" className="col-md-6 col-lg-3" />
+        <Views className="col-md-6 col-lg-3" />
+        <Card title="Work preferences" content="Graphic Designer / Desktop Publisher (Intern)" type="specialization" className="col-md-6 col-lg-3" />
+        <Card title="Additional info" content="Help recruiters get to know you better by describing your skills" type="course" className="col-md-6 col-lg-3" />
       </div>
-      <div className={`${styles.statistics} row`}>
+      {/* <div className={`${styles.statistics} row`}>
         <Card title="1,921 views per week" type="statistic" className="col-md-6 col-lg-3" />
         <Card title="306 bonuses for education" type="statistic" className="col-md-6 col-lg-3" />
         <Card title="26 courses started" type="statistic" className="col-md-6 col-lg-3" />
         <Card title="35 courses in favorites" type="statistic" className="col-md-6 col-lg-3" />
-      </div>
+      </div> */}
       <div className={`${styles.workPreferences} row`}>
         <Card title="Work preferences" content="Graphic Designer / Desktop Publisher (Intern)" className="col-md-6 col-lg-3" />
         <Card title="Additional info" content="Help recruiters get to know you better by describing your skills" className="col-md-6 col-lg-3" />
       </div>
       <div className={`${styles.resume} row`}>
-        <button className="btn btn-secondary col-md-6 col-lg-3">Resume and portfolio</button>
-        <button className="btn btn-secondary col-md-6 col-lg-3">Download certificates</button>
+        <button className="btn btn-danger col-md-6 col-lg-3">Download certificates</button>
+        <button className="btn btn-outline col-md-6 col-lg-3 border border-1 border-danger text-danger">Resume and portfolio</button>
       </div>
     </div>
   );
--- src/components/Dashboard/common/Card/Card.jsx
+++ src/components/Dashboard/common/Card/Card.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import styles from './Card.module.css';
+import { BsArrowUpRightCircle } from "react-icons/bs";
 
 const Card = ({ title, type, content }) => {
   return (
     <div className={`${styles.card} ${styles[type]}`}>
-      <h3>{title}</h3>
-      {content && <p>{content}</p>}
-      {type !== 'statistic' && <button className="btn btn-primary">Continue</button>}
+       <div className="row justify-content-between align-items-center mb-3">
+        <div className="col-auto">
+        <p className={`${styles.account}`}>desired roles</p>
+        </div>
+        <div className="col-auto">
+          <BsArrowUpRightCircle className={styles.headerIcon} />
+        </div>
+      </div>
+      { <div> <p>{title}</p> <p>{content}</p></div>}
     </div>
   );
 };
