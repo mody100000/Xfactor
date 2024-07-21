d6cb75a4a96f99c4616415008881191e26673ed2
Mahmoud Mohamed Gomaa <m.anamody10000@gmail.com>
2024-07-21 11:38:00 +0300

    fix the sidebar toggle issue

--- src/components/Chat/ChatSidebar/ChatSidebar.jsx
+++ src/components/Chat/ChatSidebar/ChatSidebar.jsx
@@ -2,36 +2,23 @@ import React from 'react';
 import styles from './ChatSidebar.module.css';
 import user from "@assets/user.jpeg"
 const contacts = [
-    { name: 'Mark William', img: user, message: "Count me in then! I can't wait to...", time: '05:18' },
-    { name: 'Benjamin Hughes', img: user, message: "Watch your creativity bloom with...", time: '05:15' },
-    { name: 'Olivia Patel', img: user, message: "Explore the vibrant realm of water...", time: '05:10' },
-    { name: 'Jack Anderson', img: user, message: "Step into the realm of magical re...", time: '05:05' },
-    { name: 'Jack Anderson', img: user, message: "Step into the realm of magical re...", time: '05:05' },
-    { name: 'Jack Anderson', img: user, message: "Step into the realm of magical re...", time: '05:05' },
-    { name: 'Jack Anderson', img: user, message: "Step into the realm of magical re...", time: '05:05' },
-    { name: 'Jack Anderson', img: user, message: "Step into the realm of magical re...", time: '05:05' },
-
-    { name: 'Emily Reynolds', img: user, message: "Embark on a biking adventure that...", time: '05:00' },
-    { name: 'Matthew Campbell', img: user, message: "Count me in then! I can't wait to...", time: '04:12' },
-    { name: 'Matthew Campbell', img: user, message: "Count me in then! I can't wait to...", time: '04:12' },
-    { name: 'Matthew Campbell', img: user, message: "Count me in then! I can't wait to...", time: '04:12' },
-    { name: 'Matthew Campbell', img: user, message: "Count me in then! I can't wait to...", time: '04:12' },
-    { name: 'Matthew Campbell', img: user, message: "Count me in then! I can't wait to...", time: '04:12' },
-    { name: 'Matthew Campbell', img: user, message: "Count me in then! I can't wait to...", time: '04:12' },
-    { name: 'Matthew Campbell', img: user, message: "Count me in then! I can't wait to...", time: '04:12' },
-
-    { name: 'Christian Buehner', img: user, message: "Discover the art of mindfulness...", time: '04:09' }
+    { id: 1, name: 'Mark William', img: user, message: "Count me in then! I can't wait to...", time: '05:18' },
+    { id: 2, name: 'Benjamin Hughes', img: user, message: "Watch your creativity bloom with...", time: '05:15' },
+    { id: 3, name: 'Olivia Patel', img: user, message: "Explore the vibrant realm of water...", time: '05:10' },
+    { id: 4, name: 'Jack Anderson', img: user, message: "Step into the realm of magical re...", time: '05:05' },
+    { id: 5, name: 'Emily Reynolds', img: user, message: "Embark on a biking adventure that...", time: '05:00' },
+    { id: 6, name: 'Matthew Campbell', img: user, message: "Count me in then! I can't wait to...", time: '04:12' },
+    { id: 7, name: 'Christian Buehner', img: user, message: "Discover the art of mindfulness...", time: '04:09' }
 ];
 
 const ChatSidebar = ({ isSidebarOpen, toggleSidebar }) => {
     return (
         <div className={`${styles.sidebar} ${isSidebarOpen ? styles.open : ''}`}>
-            <button className={`btn btn-secondary d-md-none ${styles.closeBtn}`} onClick={toggleSidebar}>Close</button>
             <div className='p-3'>
                 <input type="text" className={`form-control mb-3 ${styles.searchInput}`} placeholder="Search" />
             </div>
-            {contacts.map((contact, index) => (
-                <div key={index} className={`${styles.contact} mb-3 p-2`}>
+            {contacts.map((contact) => (
+                <div key={contact.id} className={`${styles.contact} mb-3 p-2`} onClick={toggleSidebar}>
                     <div>
                         <img src={contact.img} alt="" className={styles.userImg} />
                     </div>
