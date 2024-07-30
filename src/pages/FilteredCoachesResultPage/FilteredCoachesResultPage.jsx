caa709f8c7e8deb0b2771a32a8fc683a7ffda79b
Mahmoud Mohamed Gomaa <m.anamody10000@gmail.com>
2024-07-30 16:16:51 +0300

    redesign FilteredCoachesResult

--- src/pages/FilteredCoachesResultPage/FilteredCoachesResultPage.jsx
+++ src/pages/FilteredCoachesResultPage/FilteredCoachesResultPage.jsx
@@ -3,6 +3,7 @@ import { useLocation, Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import styles from './FilteredCoachesResultPage.module.css';
 import coachImage from '../../assets/s3.jpg';
+import { IoMdAdd } from "react-icons/io";
 
 const FilteredCoachesResultPage = () => {
   const sport = useSelector((state) => state.coach.sport);
@@ -97,85 +98,96 @@ const FilteredCoachesResultPage = () => {
 
   return (
     <>
-        <div className={styles.intro}>
+      <div className={styles.intro}>
         <div className={styles.overlay}>
-            <div className='d-flex flex-column align-items-center justify-content-center p-5'>
+          <div className='d-flex flex-column align-items-center justify-content-center p-5'>
             <h1 className="fw-bold">Coaches</h1>
             <h3 className={`text-center ${styles.suptitle}`}>
-            Recommended {sport} coaches near {address}
+              Recommended {sport} coaches near {address}
             </h3>
-            </div>
-           </div>
-            <div>
+          </div>
         </div>
       </div>
-    <div className={styles.container}>
-      <div className={styles.top}>
-        <div className={styles.filters}>
-          <label>
-            <select name='Price Range:' className={styles.filterInbut}  onChange={handlePriceRangeChange}>
-             <option value=""  style={{ display: 'none' }}>Price Range:</option>
-              <option value="0-25">0 - 25</option>
-              <option value="26-50">26 - 50</option>
-              <option value="51-75">51 - 75</option>
-              <option value="76-100">76 - 100</option>
-              <option value="101-9999">100+</option>
-            </select>
-          </label>
-          <label>
-            <select className={styles.filterInbut} onChange={handleGenderFilterChange}>
-             <option value=""  style={{ display: 'none' }}>Gender:</option>
-              <option value="Any">Any</option>
-              <option value="Male">Male</option>
-              <option value="Female">Female</option>
-            </select>
-          </label>
-          <label>
-            
-            <select className={styles.filterInbut} onChange={handleTrainingTypeChange}>
-             <option value=""  style={{ display: 'none' }}>Training Type:</option>
-              <option value="All">All</option>
-              <option value="In-Person">In-Person</option>
-              <option value="Online">Online</option>
-            </select>
-          </label>
-          <label>
-            
-            <select className={styles.filterInbut} onChange={handleSortChange}>
-             <option value=""  style={{ display: 'none' }}>Sort By:</option>
-              <option value="Recommended">Recommended</option>
-              <option value="Price">Price</option>
-            </select>
-          </label>
+      <div className={styles.container}>
+        <div className={styles.top}>
+          <div className={styles.filters}>
+            <label>
+              <select name='Price Range:' className={styles.filterInbut} onChange={handlePriceRangeChange}>
+                <option value="" style={{ display: 'none' }}>Price Range:</option>
+                <option value="0-25">0 - 25</option>
+                <option value="26-50">26 - 50</option>
+                <option value="51-75">51 - 75</option>
+                <option value="76-100">76 - 100</option>
+                <option value="101-9999">100+</option>
+              </select>
+            </label>
+            <label>
+              <select className={styles.filterInbut} onChange={handleGenderFilterChange}>
+                <option value="" style={{ display: 'none' }}>Gender:</option>
+                <option value="Any">Any</option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+              </select>
+            </label>
+            <label>
+              <select className={styles.filterInbut} onChange={handleTrainingTypeChange}>
+                <option value="" style={{ display: 'none' }}>Training Type:</option>
+                <option value="All">All</option>
+                <option value="In-Person">In-Person</option>
+                <option value="Online">Online</option>
+              </select>
+            </label>
+            <label>
+              <select className={styles.filterInbut} onChange={handleSortChange}>
+                <option value="" style={{ display: 'none' }}>Sort By:</option>
+                <option value="Recommended">Recommended</option>
+                <option value="Price">Price</option>
+              </select>
+            </label>
+          </div>
         </div>
-      </div>
-      <div className={styles.coachesContainer}>
-        {currentCoaches.length > 0 ? (
-          currentCoaches.map((coach) => (
-            <div key={coach.id} className={`${styles.coachCard} ${coach.recommended ? styles.recommended : ''}`}>
-              <img src={coach.image || coachImage} alt={coach.name} className={styles.coachImage} />
-              <h2 className={styles.coachName}>{coach.name}</h2>
-              <p className={styles.recommendedText}>{coach.recommended?"recommended":null}</p>
-              <p className={styles.coachCategory}>{coach.category}</p>
-              <div className={styles.rating}>
-                <span className={styles.stars}>{'★'.repeat(coach.rating)}{'☆'.repeat(5 - coach.rating)}</span>
-                <span className={styles.reviews}>{coach.reviews} reviews</span>
+        <div className='row'>
+          {currentCoaches.length > 0 ? (
+            currentCoaches.map((coach) => (
+              <div key={coach.id} className={`col-md-6 ${styles.coachColumn}`}>
+                <div className={styles.coachCard}>
+                  <div className={styles.coachMain}>
+                    <div className='d-flex gap-3'>
+                      <img src={coach.image || coachImage} alt={coach.name} className={styles.coachImage} />
+                      <div className={styles.coachDetails}>
+                        <h2 className={styles.coachName}>{coach.name}</h2>
+                        <div className={styles.rating}>
+                          <span className={styles.stars}>{'★'.repeat(coach.rating)}{'☆'.repeat(5 - coach.rating)}</span>
+                          <span className={styles.reviews}>{coach.reviews} reviews</span>
+                        </div>
+                        <p className={styles.coachCategory}>{coach.category}</p>
+                      </div>
+                    </div>
+                    <p className=''>{coach.trainingOfferIcon}{coach.trainingOffer}</p>
+                    <p className={styles.summary}>{coach.summary}</p>
+                    <p className={styles.distance}><span className='fw-bold'>{coach.distance}</span> miles away from {address}</p>
+                  </div>
+                  <div className={styles.coachInfo}>
+                    <span className={`${styles.badge} ${getBadgeClass(coach.badge)}`}>{coach.badge}</span>
+                    <p className='mt-3 mb-1 fs-5'>Starting At</p>
+                    <p className='my-1'><span className='fs-4'>$</span><span className={styles.salary}>{coach.salary}</span>/session</p>
+                    <IoMdAdd size={25} />
+                    <p className={styles.fee}>Applicable Fees</p>
+                    <Link to={`/coach/${coach.id}`} className={styles.viewProfileLink}>
+                      <button className={styles.viewProfileButton}>View Profile</button>
+                    </Link>
+                  </div>
+                </div>
               </div>
-              <span className={`${styles.badge} ${getBadgeClass(coach.badge)}`}>{coach.badge}</span>
-              <p className={styles.summary}>{coach.summary}</p>
-              <p className={styles.distance}>{coach.distance} miles away from {address}</p>
-              <p className={styles.salary}>${coach.salary}/session</p>
-              <Link to={`/coaches/${coach.id}`} className={styles.viewProfileButton}>View Profile</Link>
-            </div>
-          ))
-        ) : (
-          <p className={styles.noResults}>No coaches found matching your criteria.</p>
-        )}
+            ))
+          ) : (
+            <p className={styles.noResults}>No coaches found matching your criteria.</p>
+          )}
+        </div>
+        <div className={styles.pagination}>
+          {renderPagination()}
+        </div>
       </div>
-      <div className={styles.pagination}>
-        {renderPagination()}
-      </div>
-    </div>
     </>
   );
 };
