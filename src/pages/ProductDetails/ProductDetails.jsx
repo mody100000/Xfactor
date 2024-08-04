afd83b02448488b3c00be63d0a4bcb00b66084bf
Mahmoud Mohamed Gomaa <m.anamody10000@gmail.com>
2024-08-04 12:32:58 +0300

    make productDetails page responsive for all screens

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -7,7 +7,7 @@ import styles from './ProductDetails.module.css';
 const ProductDetails = () => {
   const { id } = useParams();
   const product = productsData.find(product => product.id === parseInt(id));
-  const [quantity, setQuantity] = useState(1); // Initialize quantity state
+  const [quantity, setQuantity] = useState(1);
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -15,13 +15,14 @@ const ProductDetails = () => {
 
   const navigate = useNavigate();
 
-  const handelNavigate = ()=>{
-   navigate("/cart")
+  const handelNavigate = () => {
+    navigate("/cart")
   }
 
   if (!product) {
     return <div>Product not found</div>;
   }
+
   const handleIncrease = () => {
     setQuantity(quantity + 1);
   };
@@ -35,40 +36,35 @@ const ProductDetails = () => {
   return (
     <div className={`container-fluid ${styles.productDetailsPage}`}>
       <div className="row">
-        {/* Right Column for Images */}
-        <div className={`col-md-7 col-12 ${styles.rightColumn}`}>
-          <div className="row">
-            <div className="col-md-3 col-12">
-              <div className={styles.thumbnails}>
-                {product.thumbnails.map((thumb, index) => (
-                  <img key={index} src={thumb} alt={`Thumbnail ${index}`} className={styles.thumbnail} />
-                ))}
-              </div>
-            </div>
-            <div className="col-md-9 col-12">
-              <img src={product.image} alt={product.title} className={styles.mainImage} />
+        <div className="col-12 col-md-7">
+          <div className={styles.imageContainer}>
+            <img src={product.image} alt={product.title} className={styles.mainImage} />
+            <div className={styles.thumbnails}>
+              {product.thumbnails.map((thumb, index) => (
+                <img key={index} src={thumb} alt={`Thumbnail ${index}`} className={styles.thumbnail} />
+              ))}
             </div>
           </div>
         </div>
 
-        {/* Left Column for Product Info */}
-        <div className={`col-md-5 col-12 ${styles.leftColumn}`}>
+        <div className={`col-12 col-md-5 ${styles.leftColumn}`}>
           <div className={styles.productInfo}>
             <h1 className={styles.productTitle}>{product.title}</h1>
             <p className={styles.productPrice}>{product.price}</p>
             <p className={styles.productDescription}>{product.info}</p>
-            <div className={styles.quantityContainer}>
-              <button className={styles.quantityButton} onClick={handleDecrease}>-</button>
-              <input type="text" value={quantity} className={styles.quantityInput} readOnly />
-              <button className={styles.quantityButton} onClick={handleIncrease}>+</button>
+            <div className={styles.quantityAndCartContainer}>
+              <div className={styles.quantityContainer}>
+                <button className={styles.quantityButton} onClick={handleDecrease}>-</button>
+                <input type="text" value={quantity} className={styles.quantityInput} readOnly />
+                <button className={styles.quantityButton} onClick={handleIncrease}>+</button>
+              </div>
+              <button className={styles.addToCartButton} onClick={handelNavigate}>Add To Cart</button>
             </div>
-            <button className={styles.addToCartButton} onClick={handelNavigate}>Add To Cart</button>
           </div>
         </div>
       </div>
       <span className={styles.line}></span>
 
-      {/* Descriptions Section */}
       <div className={styles.productDetailsSection}>
         <h2>Descriptions</h2>
         <p>Lorem ipsum dolor sit amet consectetur. Magna dui tincidunt mi amet. Nulla massa mauris tristique auctor. Fermentum sit eget pulvinar mi amet gravida nulla. Velit enim neque quam purus.Lorem ipsum dolor sit amet consectetur. Magna dui tincidunt mi amet. Nulla massa mauris tristique auctor. Fermentum sit eget pulvinar mi amet gravida nulla. Velit enim neque quam purus. </p>
