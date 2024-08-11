1a92b1ca11b087a15cf62c445290e0c11527a16c
Mahmoud Mohamed Gomaa <m.anamody10000@gmail.com>
2024-08-11 15:08:12 +0300

    adding CoachPackages component to CoachDetails Page

--- src/components/CoachDetailsComponents/MainCoachDetails/MainCoachDetails.jsx
+++ src/components/CoachDetailsComponents/MainCoachDetails/MainCoachDetails.jsx
@@ -1,9 +1,12 @@
+import CoachPackages from "./CoachPackages/CoachPackages";
 import styles from "./MainCoachDetails.module.css"
 import coachImage from "@assets/user.webp"
+import { MdOutlineVerifiedUser } from "react-icons/md";
+
 const MainCoachDetails = () => {
     return (
         <div className={styles.container}>
-            <div className="d-flex flex-column justify-content-center align-items-center">
+            <div className="d-flex flex-column justify-content-center">
                 <div className={styles.coachCard}>
                     <div className={styles.coachMain}>
                         <div className='d-flex gap-3'>
@@ -25,13 +28,19 @@ const MainCoachDetails = () => {
                         </div>
                         <div className='d-flex flex-column'>
                             <p className={styles.summary}>Professional trainer, experience with athletes ranging from pro to youth. As a former pro athlete, I understand what it takes to reach your fitness goals. Sports Science degree</p>
-                            <div className={styles.verified}>
-                                <p className={styles.distance}>Background Check verified</p>
-                                <p className={styles.textLink}>Learn More</p>
+                            <div className={styles.verifiedUser}>
+                                <div>
+                                    <MdOutlineVerifiedUser size={40} />
+                                </div>
+                                <div className={styles.verified}>
+                                    <p className={styles.distance}>Background Check verified</p>
+                                    <p className={styles.textLink}>Learn More</p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
+                <CoachPackages />
             </div>
         </div>
     );
