808e61952d329fc0d64d85a4feca5e6dd423cc25
Mahmoud Mohamed Gomaa <m.anamody10000@gmail.com>
2024-08-12 12:31:06 +0300

    get the MainCoachDetails data from the coaches object

--- src/components/CoachDetailsComponents/MainCoachDetails/MainCoachDetails.jsx
+++ src/components/CoachDetailsComponents/MainCoachDetails/MainCoachDetails.jsx
@@ -3,40 +3,44 @@ import styles from "./MainCoachDetails.module.css"
 import coachImage from "@assets/user.webp"
 import { MdOutlineVerifiedUser } from "react-icons/md";
 
-const MainCoachDetails = () => {
+const MainCoachDetails = ({ coach }) => {
     return (
         <div className={styles.container}>
             <div className="d-flex flex-column justify-content-center">
                 <div className={styles.coachCard}>
                     <div className={styles.coachMain}>
                         <div className='d-flex gap-3'>
-                            <img src={coachImage} alt="John Doe" className={styles.coachImage} />
+                            <img src={coach.image || coachImage} alt={coach.name} className={styles.coachImage} />
                             <div className="px-3">
-                                <h2 className={styles.coachName}>John Doe</h2>
+                                <h2 className={styles.coachName}>{coach.name}</h2>
                                 <div className={styles.rating}>
-                                    <span className={styles.stars}>{'★'.repeat(5)}{'☆'.repeat(5 - 3)}</span>
-                                    <span className={styles.reviews}>120 reviews</span>
+                                    <span className={styles.stars}>{'★'.repeat(coach.rating)}{'☆'.repeat(5 - coach.rating)}</span>
+                                    <span className={styles.reviews}>{coach.reviews} Reviews</span>
                                 </div>
-                                <select
-                                    className={styles.dropdown}
-                                >
-                                    <option value="" style={{ display: 'none' }}>Select...</option>
-                                    <option value="1">1</option>
-                                    <option value="2-4">2-4</option>
+                                <select className={styles.dropdown}>
+                                    {coach.categories.map((category, index) => (
+                                        <option key={index} value={category}>
+                                            {category}
+                                        </option>
+                                    ))}
                                 </select>
+
                             </div>
                         </div>
                         <div className='d-flex flex-column'>
-                            <p className={styles.summary}>Professional trainer, experience with athletes ranging from pro to youth. As a former pro athlete, I understand what it takes to reach your fitness goals. Sports Science degree</p>
-                            <div className={styles.verifiedUser}>
-                                <div>
-                                    <MdOutlineVerifiedUser size={40} />
-                                </div>
-                                <div className={styles.verified}>
-                                    <p className={styles.distance}>Background Check verified</p>
-                                    <p className={styles.textLink}>Learn More</p>
-                                </div>
-                            </div>
+                            <p className={styles.summary}>{coach.summary} <span className={styles.textLink}>View All Coaching Experience</span></p>
+
+                            {coach.verified ?
+                                <div className={styles.verifiedUser}>
+                                    <div>
+                                        <MdOutlineVerifiedUser size={40} />
+                                    </div>
+                                    <div className={styles.verified}>
+                                        <p className={styles.distance}>Background Check verified</p>
+                                        <p className={styles.textLink}>Learn More</p>
+                                    </div>
+                                </div> : null}
+
                         </div>
                     </div>
                 </div>
