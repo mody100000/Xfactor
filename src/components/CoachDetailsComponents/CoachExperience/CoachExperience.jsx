e0fa36b0755fc835456d0983b7f2e6a1dcecc523
Mahmoud Mohamed Gomaa <m.anamody10000@gmail.com>
2024-08-26 11:49:57 +0300

    replace the coach static data to dynamic one and enhance some features

--- src/components/CoachDetailsComponents/CoachExperience/CoachExperience.jsx
+++ src/components/CoachDetailsComponents/CoachExperience/CoachExperience.jsx
@@ -6,9 +6,9 @@ const CoachExperience = ({ coach }) => {
   const [activeTab, setActiveTab] = useState('experience');
 
   const tabContent = {
-    experience: "I have played sports like baseball, football, basketball and boxing. I have played football from age 8 up to age 18, were I learned to play both side of the ball. I have played youth and semi pro football. I have been coaching football on two levels one as a semi pro and the other as a youth coach. I started coaching semi pro in 1994 up until 2007. and I started coaching youth football in 1996. at both levels I coach defense , I attend Glazer football Clinic an I'm USA coach certified. I still coach youth football now for the Linden Tigers. I also coach baseball for the linden PAL , which I have been doing for 3years now. Before than II coached in East Orange Little League two years, and Newark Little League for 3 years. I now have my own double A adult baseball team which has play in 3 se",
-    highlights: "Won 5 championship and 6 division championship these are with both semi pro and youth football. I have some of my players go on to play in the NFL and a few other play arena football.l I have taught players skill and technique to help them develop in this sport they play.. I also know Baseball, basketball, and boxing as well.",
-    plan: "First I will talk to my client to see where he/ she is having problem or need up. After that I will have him or her warm up than we will go through some drill. than we will work on the problem, Than we will put everything together.exploding out of your stand to receive the ball . Than we will work on the receiving the ball from the quarterback. Learn the holes and the different way offensive use them. You will learn how to come out of the back field, running your ro out of the back field.utes to catch a pass."
+    experience: coach.experience,
+    highlights: coach.highlights,
+    plan: coach.plan
   };
 
   const isMobile = window.innerWidth <= 500;
@@ -19,7 +19,7 @@ const CoachExperience = ({ coach }) => {
       {isMobile ? (
         <Dropdown>
           <Dropdown.Toggle className={styles.customDropdownToggle}>
-            {activeTab === 'experience' && 'Coach Experience'}
+            {activeTab === coach.experience && 'Coach Experience'}
             {activeTab === 'highlights' && 'Athletic Highlights'}
             {activeTab === 'plan' && 'Session Plan'}
           </Dropdown.Toggle>
--- src/components/LandingNav/LandingNav.jsx
+++ src/components/LandingNav/LandingNav.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import { IoLogIn } from 'react-icons/io5';
@@ -16,54 +16,61 @@ import { CiDark } from 'react-icons/ci';
 import { TiThMenu } from "react-icons/ti";
 import { BsFilePerson } from "react-icons/bs";
 import logoLightMode from "@assets/logoLightMode.png"
+
 const LogoutNav = ({ theme, toggleTheme }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const closeNav = () => setExpanded(false);
+
   return (
     <>
       <Navbar
         collapseOnSelect
         expand="xl"
         className={`py-2 px-1 px-md-3 ${styles.navbar} ${theme === 'dark' ? 'navbar-dark bg-dark' : 'navbar-light bg-light'}`}
+        expanded={expanded}
+        onToggle={(expanded) => setExpanded(expanded)}
       >
         <Navbar.Brand>
-          <Link to="/" className={styles.brand}>
+          <Link to="/" className={styles.brand} onClick={closeNav}>
             {theme === 'dark' ?
               <img src={logo} className={styles.logo} alt="XFACTOR" /> : <img src={logoLightMode} className={styles.logo} alt="XFACTOR" />}
           </Link>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav"  ><TiThMenu className={styles.toggle} size={25} /></Navbar.Toggle>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav"><TiThMenu className={styles.toggle} size={25} /></Navbar.Toggle>
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto d-flex justify-content-center align-items-center">
-            <Link to="/filtered-coaches" className={styles.item_links}>
+            <Link to="/filtered-coaches" className={styles.item_links} onClick={closeNav}>
               See Our Coaches <BsFilePerson size={20} />
             </Link>
-            <Link to="/CoachUp" className={styles.item_links}>
+            <Link to="/CoachUp" className={styles.item_links} onClick={closeNav}>
               CoachUp Works <GrWorkshop size={20} />
             </Link>
-            <Link to="/blogs" className={styles.item_links}>
+            <Link to="/blogs" className={styles.item_links} onClick={closeNav}>
               Blogs <RxReader size={20} />
             </Link>
-            <Link to="/FAQ" className={styles.item_links}>
+            <Link to="/FAQ" className={styles.item_links} onClick={closeNav}>
               Questions <FaRegCircleQuestion size={20} />
             </Link>
-            <Link to="/AboutUs" className={styles.item_links}>
+            <Link to="/AboutUs" className={styles.item_links} onClick={closeNav}>
               About Us <MdRealEstateAgent size={20} />
             </Link>
-            <Link to="/contactUs" className={styles.item_links}>
+            <Link to="/contactUs" className={styles.item_links} onClick={closeNav}>
               Contact Us <MdOutlineContactPhone size={23} />
             </Link>
-            <Link to="/cart" className={styles.item_links}>
+            <Link to="/cart" className={styles.item_links} onClick={closeNav}>
               Cart <TbShoppingBag size={23} />
             </Link>
-            <Link to="/shop" className={styles.item_links}>
+            <Link to="/shop" className={styles.item_links} onClick={closeNav}>
               Shop <FiShoppingCart size={23} />
             </Link>
-            <Link to="/registerOptions" className={styles.item_links}>
+            <Link to="/registerOptions" className={styles.item_links} onClick={closeNav}>
               Register <RiUserAddFill size={20} />
             </Link>
-            <Link to="/login" className={styles.item_links}>
+            <Link to="/login" className={styles.item_links} onClick={closeNav}>
               Login <IoLogIn size={25} />
             </Link>
-            <div className={styles.item_links} onClick={toggleTheme}>
+            <div className={styles.item_links} onClick={() => { toggleTheme(); closeNav(); }}>
               Dark Mode  <CiDark className={styles.darkModeIcon} size={30} />
             </div>
           </Nav>
