6d410bc17ac483da22ff7edf9f7964ea05845efc
Mahmoud Mohamed Gomaa <m.anamody10000@gmail.com>
2024-10-22 13:37:19 +0300

    add ScheduleSessions to the client dashboard

---
+++ src/pages/ClientDashboard/Sessions/Sessions.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import styles from "./Sessions.module.css";
+import { SlCalender } from "react-icons/sl";
+import CoursesSidebar from '../../../components/DashboardCourses/CoursesSidebar/CoursesSidebar';
+import ScheduleSessions from '../../../components/ClientDashboardComponents/SessionComponents/ScheduleSessions';
+const Sessions = () => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+    };
+
+    return (
+        <div className={`container-fluid ${styles.dashboardCoursesPage}`}>
+            {/* Toggle button for small screens */}
+            <button className={styles.toggleBtn} onClick={toggleSidebar}><SlCalender size={26} /></button>
+
+            <div className="row">
+                {/* Main Content */}
+                <div className={`col-12 col-lg-8 ${styles.mainContent}`}>
+                    <ScheduleSessions />
+                    {/* <Courses /> */}
+                </div>
+
+                {/* Right Sidebar */}
+                <div className={`col-12 col-lg-4 ${styles.rightSidebar} ${isSidebarOpen ? styles.open : ''}`}>
+                    <CoursesSidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Sessions;
