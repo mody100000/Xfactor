cdab03b5e5608ff305b7544b78522f9ffed60b32
Mahmoud Mohamed Gomaa <m.anamody10000@gmail.com>
2025-01-14 21:00:49 +0200

    enhance  clients cards layout  in ClientFilter page

--- src/components/ClientsData/ClientsData.js
+++ src/components/ClientsData/ClientsData.js
@@ -2,17 +2,24 @@ import event1 from "@assets/user.webp";
 import event2 from "@assets/RegisterCoach (1).jpg";
 import event3 from "@assets/review2.jpeg";
 import event4 from "@assets/review1.jpeg";
+import { GrYoga } from "react-icons/gr";
+import { IoTennisballOutline } from "react-icons/io5";
+import { TbSwimming } from "react-icons/tb";
+import { IoIosFootball } from "react-icons/io";
 
 export const clientData = [
   {
     id: 1,
     coachName: "Emily",
+    coachLastName: "Harper",
     sessionType: "Yoga",
     sessionLocation: "Studio",
+    // sessionLogo: GrYoga,
     img: event1,
     description:
       "Experienced yoga instructor specializing in mindfulness and stress reduction techniques. Offers personalized sessions for all skill levels.",
     time: "10:00AM",
+    pendingSessions: 4,
     upcomingSessions: [
       {
         id: 101,
@@ -40,12 +47,17 @@ export const clientData = [
   {
     id: 2,
     coachName: "Mark",
+    coachLastName: "Jackson",
+
     sessionType: "Tennis",
     sessionLocation: "Online",
+    // sessionLogo: IoTennisballOutline,
     img: event3,
     description:
       "Professional tennis coach with years of competitive experience. Provides technique improvement and strategic training.",
     time: "1:30PM",
+    pendingSessions: 4,
+
     upcomingSessions: [
       {
         id: 201,
@@ -73,13 +85,17 @@ export const clientData = [
   {
     id: 3,
     coachName: "Jhin",
+    coachLastName: "Mason",
     sessionType: "Football",
     sessionLocation: "Online",
+    // sessionLogo: IoIosFootball,
     img: event4,
     description:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore ipsam commodi odit molestiae debitis, maiores tempore in! Dolorum, fugit obcaecati! Voluptates provident eius ad laudantium exercitationem, vel corrupti aliquid quo?",
     time: "8:30PM",
     date: new Date(2025, 1, 28),
+    pendingSessions: 4,
+
     upcomingSessions: [
       {
         id: 301,
@@ -96,6 +112,7 @@ export const clientData = [
         coachName: "Mark",
         sessionType: "Tennis",
         sessionLocation: "Online",
+        // sessionLogo: IoTennisballOutline,
         img: event2,
         description:
           "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis dolore error eveniet voluptates maiores quasi ab quam accusantium expedita, et in eligendi officia vitae aut libero consequuntur accusamus voluptas numquam.",
@@ -107,16 +124,29 @@ export const clientData = [
   {
     id: 4,
     coachName: "Taric",
+    coachLastName: "Addison",
+
     sessionType: "Swimming",
     sessionLocation: "Campus",
+    // sessionLogo: TbSwimming,
     img: event3,
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem in error rem eveniet, quisquam et eaque. Rem ducimus, quo accusantium possimus veritatis, quos dolores reiciendis et dignissimos fugiat cupiditate enim!",
     time: "5:30PM",
     date: new Date(2025, 1, 5),
+    pendingSessions: 4,
+
     upcomingSessions: [
       {
         id: 401,
+        img: event1,
+        description:
+          "Experienced yoga instructor specializing in mindfulness and stress reduction techniques. Offers personalized sessions for all skill levels.",
+        time: "10:00AM",
+        date: new Date(2025, 1, 15),
+      },
+      {
+        id: 432,
         img: event1,
         description:
           "Experienced yoga instructor specializing in mindfulness and stress reduction techniques. Offers personalized sessions for all skill levels.",
@@ -130,6 +160,7 @@ export const clientData = [
         coachName: "Mark",
         sessionType: "Tennis",
         sessionLocation: "Online",
+        // sessionLogo: IoTennisballOutline,
         img: event2,
         description:
           "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis dolore error eveniet voluptates maiores quasi ab quam accusantium expedita, et in eligendi officia vitae aut libero consequuntur accusamus voluptas numquam.",
@@ -141,13 +172,17 @@ export const clientData = [
   {
     id: 5,
     coachName: "Leo",
+    coachLastName: "Grayson",
     sessionType: "Football",
     sessionLocation: "On-site",
+    // sessionLogo: IoIosFootball,
     img: event2,
     description:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates blanditiis velit distinctio iusto nulla eos quidem quos cupiditate dolorum vel reprehenderit praesentium architecto libero excepturi esse molestiae quas, temporibus nam?",
     time: "11:30AM",
     date: new Date(2025, 1, 21),
+    pendingSessions: 4,
+
     upcomingSessions: [
       {
         id: 501,
